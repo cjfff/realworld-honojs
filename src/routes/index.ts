7668eda512206dbda03dcefc264158339e410c85
wiley <wiley@ku.com>
2024-03-24 23:33:40 +0800

    feat: add base path

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,12 @@
 // index.ts
 import { Hono } from 'hono'
+import config from '../config'
 import users from './users'
 import profiles from './profiles'
 import articles from './articles'
 import tags from './tags'
 
-const app = new Hono()
+const app = new Hono().basePath(config.basePath)
 
 app.route('/users', users)
 app.route('/profiles', profiles)
