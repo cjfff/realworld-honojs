66425b1fa626345fb6f896d54ca78773c44ff7aa
wiley <wiley@ku.com>
2024-03-26 19:44:31 +0800

    feat: handle jwt with optional parameters

--- src/routes/utils.ts
+++ src/routes/utils.ts
@@ -1,21 +1,40 @@
-import { Hono } from "hono";
-import { jwt } from 'hono/jwt'
-import type {Variables} from "./type.d";
+import { Hono, MiddlewareHandler } from "hono";
+import { verify } from "hono/jwt";
+import type { Variables } from "./type.d";
 import config from "../config";
-
 
 export const createApp = () => {
   const app = new Hono<{
-    Variables: Variables 
+    Variables: Variables;
   }>();
 
   return app;
 };
 
+export const createJWTMiddleware = (
+  verifyBoolean = true
+): MiddlewareHandler => {
+  return async (c, next) => {
+    try {
+      const tokenStr = c.req.header("Authorization");
+      if (!tokenStr && verifyBoolean) {
+        return c.json("UnAuthorized", 400);
+      }
+      const token = tokenStr?.split(" ")?.[1];
 
-export const createJWTMiddleware = () => {
-  return jwt({
-    secret: config.secret
-  })
-}
+      if (!token) {
+        return c.json("UnAuthorized", 400);
+      }
 
+      const payload = await verify(token, config.secret);
+
+      c.set("jwtPayload", payload);
+    } catch (e) {
+      if (verifyBoolean) {
+        return c.json("UnAuthorized", 400);
+      }
+    }
+
+    await next();
+  };
+};
